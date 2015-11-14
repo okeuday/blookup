c55fc4e828c25f1f5429c97110a5ccdc94ef374f
Michael Truog <mjtruog@gmail.com>
2015-11-13 21:12:42 -0800

    Add blookupf.  Rename blookup to blookupv.

---
+++ README.markdown
@@ -0,0 +1,51 @@
+Erlang Binary Lookup Data Structures
+====================================
+
+Warning: Justifying the usage of a global binary key/value dictionary
+when compared to more efficient access of data through ETS should be
+difficult (i.e., a unique circumstance).
+
+These data structures allow data to be shared between processes without
+creating separate copies of the data for each process to minimize memory usage.
+The dict API is used for the data structures (add functions as you need them).
+
+Both lookup and storage in the data structures is slow when compared to
+other Erlang data structures.  blookupf and blookupv are roughly
+27x slower than maps in 18.1 when doing lookups with a total of 10000 keys.
+The blookupf data structure is quicker to store with when compared to
+blookupv, with the speed difference increasing by a factor of 3 when
+increasing the number of keys by a factor 10 (when the number of keys is
+greater than 100 (see Note 1)).  However, blookupf has an absolute limit in
+bytes that is set upon creation (with blookupf:new/1) which limits the
+amount of data consumed by a key/value pair.
+
+Due to the slow access times, the only usage of the data structures would
+be for large amounts of data (e.g., caching of large files) to avoid a
+greater amount of latency when accessing the data without the data structures.
+
+The data structures keep all the data in large binaries due to binaries being
+the only data structure that is reference counted in Erlang.  The large
+binary usage avoids any extra copying that would create extra memory
+consumption, so memory is conserved for a large amount of data at the expense
+of slower access times.
+
+For specifying both the size of the key and the size of the value, see the
+[bisect](https://github.com/knutin/bisect) data structure.
+
+###Note 1:
+
+This assumes the blookupv stores are not replacing key/value pairs of the
+same total size.  If the sizes of the new and old key/value pairs are
+similar, set `BLOCK_SIZE_BYTES` higher to increase the chance of the
+replacement being the same total size (the difference needs to be less than the
+`BLOCK_SIZE_BYTES` value).
+
+Author
+------
+
+Michael Truog (mjtruog [at] gmail (dot) com)
+
+License
+-------
+
+BSD
--- src/blookupv.erl
+++ src/blookupv.erl
@@ -3,7 +3,7 @@
 %%%
 %%%------------------------------------------------------------------------
 %%% @doc
-%%% ==Binary Lookup Data Structure.==
+%%% ==Binary Lookup With Variably Sized Pairs.==
 %%% Keep all binary key/value data in a single continuous binary to
 %%% minimize Erlang memory usage.
 %%% @end
@@ -49,12 +49,13 @@
 %%% @version 0.1.0 {@date} {@time}
 %%%------------------------------------------------------------------------
 
--module(blookup).
+-module(blookupv).
 -author('mjtruog [at] gmail (dot) com').
 
 %% external interface
 -export([find/2,
          new/0,
+         new/1,
          store/3,
          test/0]).
 
@@ -67,6 +68,9 @@
 -ifndef(INDEX_SIZE_MAX_BYTES).
 -define(INDEX_SIZE_MAX_BYTES, 4).
 -endif.
+-ifndef(BLOCK_SIZE_BYTES).
+-define(BLOCK_SIZE_BYTES, 1). % 1 to disable blocks
+-endif.
 -define(INDEXES_ELEMENT_SIZE_BYTES,
         (?INDEX_SIZE_MAX_BYTES + ?KEY_SIZE_MAX_BYTES + ?VALUE_SIZE_MAX_BYTES)).
 -define(INDEX_SIZE_TYPE,
@@ -76,14 +80,15 @@
 -define(VALUE_SIZE_TYPE,
         :?VALUE_SIZE_MAX_BYTES/big-unsigned-integer-unit:8).
 
--record(blookup,
+-record(blookupv,
     {
         size = 0 :: non_neg_integer(),
         indexes = <<>> :: binary(),
-        data = <<>> :: binary()
+        data = <<>> :: binary(),
+        free_size :: pos_integer() | undefined
     }).
 
--type state() :: #blookup{}.
+-type state() :: #blookupv{}.
 -export_type([state/0]).
 
 %%%------------------------------------------------------------------------
@@ -97,16 +102,16 @@
 %%-------------------------------------------------------------------------
 
 -spec find(Key :: binary(),
-           Lookup :: #blookup{}) ->
+           Lookup :: #blookupv{}) ->
     {ok, Value :: binary()} |
     error.
 
 find(<<>>, _) ->
     error;
 find(Key,
-     #blookup{size = Size,
-              indexes = Indexes,
-              data = Data})
+     #blookupv{size = Size,
+               indexes = Indexes,
+               data = Data})
     when is_binary(Key) ->
     case lookup(Key, Size, Indexes, Data) of
         {true, _, Value} ->
@@ -122,10 +127,23 @@ find(Key,
 %%-------------------------------------------------------------------------
 
 -spec new() ->
-    #blookup{}.
+    #blookupv{}.
 
 new() ->
-    #blookup{}.
+    #blookupv{free_size = undefined}.
+
+%%-------------------------------------------------------------------------
+%% @doc
+%% ===Create a new binary lookup with a total size limit.===
+%% @end
+%%-------------------------------------------------------------------------
+
+-spec new(TotalSizeMaxBytes :: pos_integer()) ->
+    #blookupv{}.
+
+new(TotalSizeMaxBytes)
+    when is_integer(TotalSizeMaxBytes), TotalSizeMaxBytes > 0 ->
+    #blookupv{free_size = TotalSizeMaxBytes}.
 
 %%-------------------------------------------------------------------------
 %% @doc
@@ -135,28 +153,30 @@ new() ->
 
 -spec store(Key :: binary(),
             Value :: binary(),
-            Lookup :: #blookup{}) ->
-    #blookup{}.
+            Lookup :: #blookupv{}) ->
+    #blookupv{}.
 
 store(<<>>, _, _) ->
     erlang:exit(badarg);
 store(Key, Value,
-      #blookup{size = SizeOld,
-               indexes = IndexesOld,
-               data = DataOld} = Lookup)
+      #blookupv{size = SizeOld,
+                indexes = IndexesOld,
+                data = DataOld,
+                free_size = FreeSizeOld} = Lookup)
     when is_binary(Key), is_binary(Value) ->
-    {SizeNew, IndexesNew, DataNew} = case lookup(Key, SizeOld,
-                                                 IndexesOld, DataOld) of
+    {SizeNew, IndexesNew, DataNew,
+     FreeSizeNew} = case lookup(Key, SizeOld, IndexesOld, DataOld) of
         {true, I, _} ->
             store_key_value_replace(Key, Value, I, SizeOld,
-                                    IndexesOld, DataOld);
+                                    IndexesOld, DataOld, FreeSizeOld);
         {false, I} ->
             store_key_value_insert(Key, Value, I, SizeOld,
-                                   IndexesOld, DataOld)
+                                   IndexesOld, DataOld, FreeSizeOld)
     end,
-    Lookup#blookup{size = SizeNew,
-                   indexes = IndexesNew,
-                   data = DataNew}.
+    Lookup#blookupv{size = SizeNew,
+                    indexes = IndexesNew,
+                    data = DataNew,
+                    free_size = FreeSizeNew}.
 
 %%-------------------------------------------------------------------------
 %% @private
@@ -166,23 +186,23 @@ store(Key, Value,
 %%-------------------------------------------------------------------------
 
 test() ->
-    Lookup0 = blookup:new(),
-    error = blookup:find(<<>>, Lookup0),
-    {'EXIT', badarg} = (catch blookup:store(<<>>, <<"ignored">>, Lookup0)),
-    Lookup1 = blookup:store(<<"abcd">>, <<"value0">>, Lookup0),
-    {ok, <<"value0">>} = blookup:find(<<"abcd">>, Lookup1),
-    error = blookup:find(<<"abc">>, Lookup1),
-    Lookup2 = blookup:store(<<"abcc">>, <<"value1">>, Lookup1),
-    Lookup3 = blookup:store(<<"abcb">>, <<"value2">>, Lookup2),
-    Lookup4 = blookup:store(<<"bc">>, <<"value__3">>, Lookup3),
-    Lookup5 = blookup:store(<<"aaaaaaaaaaaa">>, <<"value00004">>, Lookup4),
-    Lookup6 = blookup:store(<<"a">>, <<"value5">>, Lookup5),
-    {ok, <<"value0">>} = blookup:find(<<"abcd">>, Lookup6),
-    {ok, <<"value2">>} = blookup:find(<<"abcb">>, Lookup6),
-    {ok, <<"value__3">>} = blookup:find(<<"bc">>, Lookup6),
-    error = blookup:find(<<"aaaaaaaaaaa">>, Lookup6),
-    {ok, <<"value00004">>} = blookup:find(<<"aaaaaaaaaaaa">>, Lookup6),
-    {ok, <<"value5">>} = blookup:find(<<"a">>, Lookup6),
+    Lookup0 = blookupv:new(1048576),
+    error = blookupv:find(<<>>, Lookup0),
+    {'EXIT', badarg} = (catch blookupv:store(<<>>, <<"ignored">>, Lookup0)),
+    Lookup1 = blookupv:store(<<"abcd">>, <<"value0">>, Lookup0),
+    {ok, <<"value0">>} = blookupv:find(<<"abcd">>, Lookup1),
+    error = blookupv:find(<<"abc">>, Lookup1),
+    Lookup2 = blookupv:store(<<"abcc">>, <<"value1">>, Lookup1),
+    Lookup3 = blookupv:store(<<"abcb">>, <<"value2">>, Lookup2),
+    Lookup4 = blookupv:store(<<"bc">>, <<"value__3">>, Lookup3),
+    Lookup5 = blookupv:store(<<"aaaaaaaaaaaa">>, <<"value00004">>, Lookup4),
+    Lookup6 = blookupv:store(<<"a">>, <<"value5">>, Lookup5),
+    {ok, <<"value0">>} = blookupv:find(<<"abcd">>, Lookup6),
+    {ok, <<"value2">>} = blookupv:find(<<"abcb">>, Lookup6),
+    {ok, <<"value__3">>} = blookupv:find(<<"bc">>, Lookup6),
+    error = blookupv:find(<<"aaaaaaaaaaa">>, Lookup6),
+    {ok, <<"value00004">>} = blookupv:find(<<"aaaaaaaaaaaa">>, Lookup6),
+    {ok, <<"value5">>} = blookupv:find(<<"a">>, Lookup6),
     ok.
 
 %%%------------------------------------------------------------------------
@@ -226,7 +246,7 @@ lookup_loop(I, I, Key, Indexes, Data) ->
             {false, I}
     end;
 lookup_loop(IMin, IMax, Key, Indexes, Data) ->
-    I = IMin + (IMax - IMin) div 2,
+    I = (IMin + IMax) div 2,
     {KeyCompare, Value} = lookup_key_value(I, Indexes, Data),
     if
         Key == KeyCompare ->
@@ -253,7 +273,8 @@ lookup_key_value(0,
                    ValueSize?VALUE_SIZE_TYPE>>,
                  Data) ->
     <<Key:KeySize/binary-unit:8,
-      Value:ValueSize/binary-unit:8>> = Data,
+      Value:ValueSize/binary-unit:8,
+      _/binary>> = Data,
     {Key, Value};
 lookup_key_value(I, Indexes, Data) ->
     Offset = I * ?INDEXES_ELEMENT_SIZE_BYTES,
@@ -268,7 +289,7 @@ lookup_key_value(I, Indexes, Data) ->
       _/binary>> = Data,
     {Key, Value}.
 
-store_key_value_replace(Key, Value, I, Size, Indexes, Data) ->
+store_key_value_replace(Key, Value, I, Size, Indexes, Data, FreeSize) ->
     Offset = I * ?INDEXES_ELEMENT_SIZE_BYTES,
     KeySizeNew = erlang:byte_size(Key),
     ValueSizeNew = erlang:byte_size(Value),
@@ -277,12 +298,22 @@ store_key_value_replace(Key, Value, I, Size, Indexes, Data) ->
       KeySizeOld?KEY_SIZE_TYPE,
       ValueSizeOld?VALUE_SIZE_TYPE,
       IndexesPart1Old/binary>> = Indexes,
-    IndexesPart1New = indexes_adjust(IndexesPart1Old,
-                                     (KeySizeNew + ValueSizeNew) -
-                                     (KeySizeOld + ValueSizeOld)),
+    {BlockSizeOld, BlockBufferBitsOld} = block_size(KeySizeOld + ValueSizeOld),
+    {BlockSizeNew, BlockBufferBitsNew} = block_size(KeySizeNew + ValueSizeNew),
+    Difference = BlockSizeNew - BlockSizeOld,
+    FreeSizeNew = if
+        FreeSize =:= undefined ->
+            undefined;
+        FreeSize >= Difference ->
+            FreeSize - Difference;
+        true ->
+            erlang:exit(badarg)
+    end,
+    IndexesPart1New = indexes_adjust(IndexesPart1Old, Difference),
     <<DataPart0:Index/binary-unit:8,
       _:KeySizeOld/binary-unit:8,
       _:ValueSizeOld/binary-unit:8,
+      0:BlockBufferBitsOld,
       DataPart1/binary>> = Data,
     {Size,
      <<IndexesPart0:Offset/binary-unit:8,
@@ -293,24 +324,46 @@ store_key_value_replace(Key, Value, I, Size, Indexes, Data) ->
      <<DataPart0:Index/binary-unit:8,
        Key:KeySizeNew/binary-unit:8,
        Value:ValueSizeNew/binary-unit:8,
-       DataPart1/binary>>}.
+       0:BlockBufferBitsNew,
+       DataPart1/binary>>,
+     FreeSizeNew}.
 
-store_key_value_insert(Key, Value, 0, 0, <<>>, <<>>) ->
+store_key_value_insert(Key, Value, 0, 0, <<>>, <<>>, FreeSize) ->
     KeySizeNew = erlang:byte_size(Key),
     ValueSizeNew = erlang:byte_size(Value),
     Index = 0,
+    {BlockSizeNew, BlockBufferBitsNew} = block_size(KeySizeNew + ValueSizeNew),
+    FreeSizeNew = if
+        FreeSize =:= undefined ->
+            undefined;
+        FreeSize >= BlockSizeNew ->
+            FreeSize - BlockSizeNew;
+        true ->
+            erlang:exit(badarg)
+    end,
     {1,
      <<Index?INDEX_SIZE_TYPE,
        KeySizeNew?KEY_SIZE_TYPE,
        ValueSizeNew?VALUE_SIZE_TYPE>>,
      <<Key:KeySizeNew/binary-unit:8,
-       Value:ValueSizeNew/binary-unit:8>>};
-store_key_value_insert(Key, Value, I, Size, Indexes, Data) ->
+       Value:ValueSizeNew/binary-unit:8,
+       0:BlockBufferBitsNew>>,
+     FreeSizeNew};
+store_key_value_insert(Key, Value, I, Size, Indexes, Data, FreeSize) ->
     Offset = I * ?INDEXES_ELEMENT_SIZE_BYTES,
     KeySizeNew = erlang:byte_size(Key),
     ValueSizeNew = erlang:byte_size(Value),
     <<IndexesPart0:Offset/binary-unit:8,
       IndexesPart1Old/binary>> = Indexes,
+    {BlockSizeNew, BlockBufferBitsNew} = block_size(KeySizeNew + ValueSizeNew),
+    FreeSizeNew = if
+        FreeSize =:= undefined ->
+            undefined;
+        FreeSize >= BlockSizeNew ->
+            FreeSize - BlockSizeNew;
+        true ->
+            erlang:exit(badarg)
+    end,
     Index = case IndexesPart1Old of
         <<>> ->
             OffsetPrevious = (I - 1) * ?INDEXES_ELEMENT_SIZE_BYTES,
@@ -318,13 +371,15 @@ store_key_value_insert(Key, Value, I, Size, Indexes, Data) ->
               IndexPrevious?INDEX_SIZE_TYPE,
               KeySizePrevious?KEY_SIZE_TYPE,
               ValueSizePrevious?VALUE_SIZE_TYPE>> = IndexesPart0,
-            IndexPrevious + KeySizePrevious + ValueSizePrevious;
+            {BlockSizePrevious, _} = block_size(KeySizePrevious +
+                                                ValueSizePrevious),
+            IndexPrevious + BlockSizePrevious;
         <<IndexValue?INDEX_SIZE_TYPE,
           _/binary>> ->
             IndexValue
     end,
     IndexesPart1New = indexes_adjust(IndexesPart1Old,
-                                     KeySizeNew + ValueSizeNew),
+                                     BlockSizeNew),
     <<DataPart0:Index/binary-unit:8,
       DataPart1/binary>> = Data,
     {Size + 1,
@@ -336,7 +391,20 @@ store_key_value_insert(Key, Value, I, Size, Indexes, Data) ->
      <<DataPart0:Index/binary-unit:8,
        Key:KeySizeNew/binary-unit:8,
        Value:ValueSizeNew/binary-unit:8,
-       DataPart1/binary>>}.
+       0:BlockBufferBitsNew,
+       DataPart1/binary>>,
+     FreeSizeNew}.
+
+block_size(BytesStored)
+    when is_integer(BytesStored) ->
+    if
+        BytesStored rem ?BLOCK_SIZE_BYTES == 0 ->
+            {BytesStored, 0};
+        true ->
+            BytesTotal = ((BytesStored div ?BLOCK_SIZE_BYTES) + 1) *
+                         ?BLOCK_SIZE_BYTES,
+            {BytesTotal, (BytesTotal - BytesStored) * 8}
+    end.
 
 indexes_adjust(<<>> = Indexes, _) ->
     Indexes;
